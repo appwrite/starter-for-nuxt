9ad24be26a47b1def9cbb1cc003851e68641cdc6
loks0n <22452787+loks0n@users.noreply.github.com>
2024-10-18 14:41:36 +0100

    chore: env

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  runtimeConfig: {
+    public: {
+      appwriteEndpoint: 'https://cloud.appwrite.io/v1',
+      appwriteProjectId: 'my-project-id',
+      appwriteProjectName: 'My Project',
+      appwriteVersion: '1.6.x',
+    },
+  },
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: [
+    '@/assets/app.css',
+    '@appwrite.io/pink',
+    '@appwrite.io/pink-icons',
+  ],
 })
