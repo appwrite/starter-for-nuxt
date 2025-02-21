7945be0560ff87d98f0dd0b9be24a514dc692d7c
Matej Bačo <matejbaco2000@gmail.com>
2025-02-21 10:44:01 +0000

    Fix ping method

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,9 @@
 export default defineNuxtConfig({
   runtimeConfig: {
     public: {
-      appwriteEndpoint: 'https://cloud.appwrite.io/v1',
-      appwriteProjectId: 'my-project-id',
-      appwriteProjectName: 'My Project',
-      appwriteVersion: '1.6.x',
+      appwriteEndpoint: process.env.NUXT_PUBLIC_APPWRITE_ENDPOINT,
+      appwriteProjectId: process.env.NUXT_PUBLIC_APPWRITE_PROJECT_ID,
+      appwriteProjectName: process.env.NUXT_PUBLIC_APPWRITE_PROJECT_NAME,
     },
   },
   compatibilityDate: '2024-04-03',
